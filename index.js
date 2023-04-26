32fd73cde167d84f91ac69f0ff9b4aa1802cb9c5
Ben Marum <ben.j.marum@gmail.com>
2023-04-26 11:40:16 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "is the champion"
+document.body.div.append(newHeader);
